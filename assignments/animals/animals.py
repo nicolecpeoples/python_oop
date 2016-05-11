3665af193b2c21a2465b95806cce64d0396b3476
Nicole Peoples <nicolecpeoples@gmail.com>
2016-05-11 08:28:08 -0700

    Adding animals assignment

---
+++ assignments/animals/animals.py
@@ -0,0 +1,46 @@
+class Animal(object):
+	def __init__(self):
+		super(Animal, self).__init__()
+		self.health = 100
+
+	def walk(self):
+		self.health -= 1
+		return self
+	def run(self):
+		self.health -= 5
+		return self
+	def displayHealth(self):
+		print(self.health)
+		return self
+
+
+class Dog(Animal):
+	def __init__(self):
+		super(Dog, self).__init__()
+
+		self.health = 150
+
+	def pet(self):
+		self.health += 5
+		return self
+
+class Dragon(Animal):
+	def __init__(self):
+		super(Dragon, self).__init__()
+		self.health = 170
+
+	def fly(self):
+		self.health -=10
+		return self
+
+	def displayHealth(self):
+		print "this is a dragon"
+		super(Dragon, self ).displayHealth()
+
+animal = Animal()
+dog = Dog()
+dog.walk().displayHealth()
+dragon = Dragon()
+dragon.walk().displayHealth()
+animal.walk().walk().walk().displayHealth()
+
--- assignments/bike/bike.py
+++ assignments/bike/bike.py
@@ -5,34 +5,27 @@ class Bike(object):
 		self.miles = 0
 	def displayInfo(self):
 		print self.price, self.max_speed, self.miles	
+		return self
 	def ride(self):
 		print "Riding"
 		self.miles +=10	
+		return self
 	def reverse(self):
 		
 		if self.miles <5:
 			print "Sorry, you can't reverse anymore. You have no more miles"
-			return "You've gotta stop riding backward you have no more miles to go!"
+			return self
 		else:
 			print "Reversing"
 			self.miles -= 5	
 
+		return self
+
 oneCoolBike = Bike(300, "40mph")
 awesomeBike = Bike(820, "64mph")
 anOkBike = Bike(95, "20mph")
-oneCoolBike.ride()
-oneCoolBike.ride()
-oneCoolBike.ride()
-oneCoolBike.reverse()			
-oneCoolBike.displayInfo()
+oneCoolBike.ride().ride().ride().reverse().displayInfo()			
 
-awesomeBike.ride()
-awesomeBike.ride()
-awesomeBike.reverse()
-awesomeBike.reverse()
-awesomeBike.displayInfo()
+awesomeBike.ride().ride().reverse().reverse().displayInfo()
 
-anOkBike.reverse()
-anOkBike.reverse()
-anOkBike.reverse()
-anOkBike.displayInfo()
+anOkBike.reverse().reverse().reverse().displayInfo()
